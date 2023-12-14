b1f6367792956c0867f957db79e44d4d176cbf63
Track1698 <autorealestate7@gmail.com>
2023-12-14 23:14:19 +0400

    v1

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def create_app():
 
          if input_kadastr:
              result = backend_function(input_kadastr)
-             print('Success:', result)
+             print('Successor:', result)
              return jsonify(result)
          else:
              return jsonify({"error": "Missing input_kadastr"}), 400
