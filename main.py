7ff8d07a82c42f733e9c51c21192cd7c2e48323c
Track1698 <autorealestate7@gmail.com>
2023-12-19 00:01:48 +0400

    Latest

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from flask_cors import CORS
 from final import backend_function
 import traceback  # Import the traceback module
 import os
+import firebase_admin
+from firebase_admin import credentials, db
+import traceback
 
 
 
+cred = credentials.Certificate("ServiceAccountK.json")
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://sakadastro-237b3-default-rtdb.europe-west1.firebasedatabase.app/'})
 
-
-    
+# Reference to the root of your Firebase Realtime Database
+db_ref = db.reference('/')
     
 def create_app():
   app = Flask(__name__)
@@ -32,9 +37,36 @@ def create_app():
  
   @app.route("/")
   def hello_world2():
-     """Example Hello World route."""
-     name = os.environ.get("NAME", "World")
-     return f"Hello {name}!"
+    return """
+        <html>
+          <head>
+              <style>
+                  body {
+                      display: flex;
+                      align-items: center;
+                      justify-content: center;
+                      height: 100vh;
+                      margin: 0;
+                      background-color: #f8f8f8;
+                  }
+
+                  h1 {
+                      color: red;
+                      font-size: 3em;
+                      text-align: center;
+                  }
+              </style>
+          </head>
+          <body>
+              <div>
+                  <h1>YOU SHOULDN'T BE HERE!!! </h1>
+              </div>
+              <div>
+                  <h1>GO AWAY OR WE HAVE YOUR IP ADDRESS!!!</h1>
+              </div>
+          </body>
+        </html>
+      """
  
   @app.route("/version", methods=["GET"], strict_slashes=False)
   def version():
@@ -45,23 +77,52 @@ def create_app():
 
 
   @app.route('/api/getinformation', methods=['POST'])
-  def your_api_endpoint():
-     try:
-         data = request.get_json()
-         input_kadastr = data.get('input_kadastr')
-         print('Looking for:', input_kadastr)
+  def getinformation():
+    try:
+        # Continue with the existing functionality
+        data = request.get_json()
+        input_kadastr = data.get('input_kadastr')
 
-         if input_kadastr:
-             result = backend_function(input_kadastr)
-             print('Success:', result)
-             return jsonify(result)
-         else:
-             return jsonify({"error": "Missing input_kadastr"}), 400
+        # Track the visitor
+        if 'X-Forwarded-For' in request.headers:
+            # The X-Forwarded-For header can contain a comma-separated list of IP addresses
+            # The client's IP address is usually the first one in the list
+            ip_address = request.headers['X-Forwarded-For'].split(',')[0].strip()
+        else:
+            # If the X-Forwarded-For header is not present, fallback to request.remote_addr
+            ip_address = request.remote_addr
+        visitors_ref = db_ref.child('visitors')
+        visitor_snapshot = visitors_ref.order_by_child('ip_address').equal_to(ip_address).get()
 
-     except Exception as e:
-         print('Error:', str(e))
-         traceback.print_exc()  # Print the traceback information
-         return jsonify({"error": str(e)}), 500
+        if not visitor_snapshot:
+            # If the visitor doesn't exist, add a new entry
+            new_visitor_ref = visitors_ref.push({
+                'ip_address': ip_address,
+                'request_count': 1,
+                'input_kadastr_list': [input_kadastr]
+            })
+        else:
+            # If the visitor exists, update the request count and input_kadastr_list
+            visitor_key = next(iter(visitor_snapshot))
+            new_visitor_ref = visitors_ref.child(visitor_key)
+            new_visitor_ref.update({
+                'request_count': visitor_snapshot[visitor_key]['request_count'] + 1,
+                'input_kadastr_list': visitor_snapshot[visitor_key].get('input_kadastr_list', []) + [input_kadastr]
+            })
+
+        print('Looking for:', input_kadastr)
+
+        if input_kadastr:
+            result = backend_function(input_kadastr)
+            print('Success:', result)
+            return jsonify(result)
+        else:
+            return jsonify({"error": "Missing input_kadastr"}), 400
+
+    except Exception as e:
+        print('Error:', str(e))
+        traceback.print_exc()  # Print the traceback information
+        return jsonify({"error": str(e)}), 500
   
   return app
 
